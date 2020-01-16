d55cea99e86042eee4a9059cf4b194580827360a
MaykonOliveira <moliveira.anschau@gmail.com>
2020-01-16 10:59:59 -0300

    Add dotenv config file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('../src/routes');
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(routes);
 
 
-mongoose.connect('mongodb+srv://deploy:I5wNYvEonEb0kFBG@cluster0-inujz.mongodb.net/developer-maps?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
